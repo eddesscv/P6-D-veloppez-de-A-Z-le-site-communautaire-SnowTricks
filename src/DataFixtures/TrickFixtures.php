c071b271582158d425d7b731941dd877f06cef72
Ednilson MENDES <ednilsonmendes@outlook.com>
2021-12-09 10:12:05 +0100

    Add and show tricks

--- src/DataFixtures/TrickFixtures.php
+++ src/DataFixtures/TrickFixtures.php
@@ -2,24 +2,117 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Image;
 use App\Entity\Trick;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Video;
+use App\Entity\Comment;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class TrickFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
     {
-        for ($i = 1; $i <= 10; $i++) {
-            $trick = new Trick();
-            $trick->setTitle("Titre de le la figure n°$i")
-                ->setDescription("<p>Contenu de la figure n°$i</p>")
-                ->setSlug("Slug n° $i")
-                ->setCreatedAt(new \DateTime());
+        $this->encoder = $encoder;
+    }
 
-            $manager->persist($trick);
+    public function load(ObjectManager $manager)
+    {
+        $faker = \Faker\Factory::create('FR-fr');
 
-            $manager->flush();
+        $users = [];
+        $categories = [];
+        $genders = ['male', 'female'];
+        $categoriesDemoName = ['Grabs', 'Rotations', 'Flips', 'Rotations désaxées', 'Slides', 'One foot', 'Old school'];
+        $tricksDemoName = ['Mute', 'Indy', '360', '720', 'Backflip', 'Misty', 'Tail slide', 'Method air', 'Backside air'];
+
+        // 3 Users
+        for ($i = 0; $i < 3; $i++) {
+            $user = new User();
+
+            $gender = $faker->randomElement($genders);
+
+            $user->setUsername($faker->userName)
+                ->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setEmail($faker->safeEmail)
+                ->setPassword($this->encoder->encodePassword($user, 'password'))
+                ->setPassword($faker->password)
+                ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                ->setActivated(true)
+                ->setImagePath('https://randomuser.me')
+                ->setImageName('api/portraits/' . ($gender == 'male' ? 'men/' : 'women/') . $faker->numberBetween(1, 99) . '.jpg')
+                ->setToken(md5(random_bytes(10)));
+            $manager->persist($user);
+            $users[] = $user;
         }
+
+        // 7 Category
+        foreach ($categoriesDemoName as $categoryTitle) {
+            $category = new Category();
+            $category->setTitle($categoryTitle);
+
+            $manager->persist($category);
+            $categories[] = $category;
+        }
+
+        // 9 Tricks
+        foreach ($tricksDemoName as $trickTitle) {
+            $trick = new Trick();
+            $trick->setTitle($trickTitle)
+                ->setDescription($faker->paragraph(5))
+                ->setCreatedAt(new \Datetime)
+                ->setUpdatedAt(new \Datetime)
+                ->setSlug($faker->slug)
+                ->setUser($faker->randomElement($users))
+                ->setCategory($faker->randomElement($categories));
+
+            // 3 Image by Trick
+            for ($k = 1; $k < 4; $k++) {
+                $image = new Image();
+                $image->setPath('img/tricks')
+                    ->setTitle($trick->getTitle() . ' ' . $k . '.jpg')
+                    //->setCaption('Image du trick ' . $trick->getName())
+                    ->setAlt('Image du trick' . $trick->getTitle())
+                    ->setTrick($trick);
+
+                $manager->persist($image);
+
+                if ($k === 3) {
+                    // Last Image become the main one
+                    $trick->setMainImage($image);
+                    $manager->persist($trick);
+                }
+            }
+
+            // 1 to 2 Video by Trick
+            for ($l = 0; $l < mt_rand(1, 2); $l++) {
+                $video = new Video();
+                $video->setUrl('https://www.youtube.com/watch?v=tHHxTHZwFUw')
+                    ->setTrick($trick);
+
+                $manager->persist($video);
+            }
+
+            // 0 to 3 Comment by Trick
+            for ($m = 0; $m < mt_rand(0, 3); $m++) {
+                $comment = new Comment();
+                $comment->setContent($faker->sentence(mt_rand(1, 5)))
+                    ->setCreatedAt(new \Datetime)
+                    ->setUser($faker->randomElement($users))
+                    ->setTrick($trick);
+
+                $manager->persist($comment);
+            }
+        }
+
+        $manager->flush();
     }
 }
--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -3,21 +3,48 @@
 namespace App\Form;
 
 use App\Entity\Trick;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class TrickType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
+        /* $builder
             ->add('title')
             ->add('description')
             ->add('slug')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title'
+            ])
+            ->add('mainImage')
             //->add('createdAt')
             //->add('updatedAt')
-        ;
+        ; */
+        $builder
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ])
+            ->add('mainImage', ImageType::class)
+            ->add('images', CollectionType::class, [
+                'entry_type' => ImageType::class,
+                'allow_add' => true,
+                'allow_delete' => true
+            ])
+            ->add('videos', CollectionType::class, [
+                'entry_type' => VideoType::class,
+                'allow_add' => true,
+                'allow_delete' => true
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
